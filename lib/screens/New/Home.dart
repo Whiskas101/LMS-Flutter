7fda2594fbfd5183779e87093bc273e5750abe26
Whiskas101 <getintouchwithvic@gmail.com>
2025-03-01 17:01:53 +0530

    bottom navigation bar with page view based navigation implemented

--- lib/screens/New/Home.dart
+++ lib/screens/New/Home.dart
@@ -1,3 +1,4 @@
+import 'package:dy_integrated_5/providers/NavStateProvider.dart';
 import 'package:dy_integrated_5/screens/Dashboard/Dashboard.dart';
 import 'package:dy_integrated_5/screens/New/AttendanceScreen.dart/AttendanceScreen.dart';
 import 'package:dy_integrated_5/screens/New/Dashboard/Dashboard.dart';
@@ -5,8 +6,9 @@ import 'package:dy_integrated_5/screens/New/SettingsScreen/SettingsScreen.dart';
 import 'package:dy_integrated_5/screens/New/VaultScreen/VaultScreen.dart';
 import 'package:dy_integrated_5/widgets/AdaptiveScaffold.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Home extends StatelessWidget {
+class Home extends ConsumerWidget {
   Home({super.key});
   final PageController _pageController = PageController();
 
@@ -14,14 +16,25 @@ class Home extends StatelessWidget {
     print("moving page: $index");
   }
 
+  void _test() {}
+
   // Helper func
-  Widget buildPageView() {
+  Widget buildPageView({WidgetRef? ref}) {
     return PageView(
       controller: _pageController,
-      onPageChanged: _handlePageChanged,
+      onPageChanged: (int value) {
+        _handlePageChanged(value);
+
+        // SYNC DETAIL
+        // if the user swipes to move to the other screens instead of clicking
+        // the bottom navigation bar items, update the provider state
+        // so the bottom state is always in sync with the current screen.
+        ref!.read(navStateProvider.notifier).setIndex(value);
+      },
       children: const <Widget>[
         DashboardV2(),
         AttendanceScreenV2(),
+        Dashboard(),
         VaultScreen(),
         SettingsScreen(),
       ],
@@ -29,10 +42,15 @@ class Home extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _currentIndex = ref.watch(navStateProvider);
+    print(":::$_currentIndex");
     return AdaptiveScaffold(
+      pageViewController: _pageController,
       body: SafeArea(
-        child: buildPageView(),
+        child: buildPageView(ref: ref),
+
+        // For the bottom navigation bar
       ),
     );
   }
--- lib/widgets/AdaptiveScaffold.dart
+++ lib/widgets/AdaptiveScaffold.dart
@@ -2,11 +2,18 @@
 
 // Wrapping stuff
 
+import 'package:dy_integrated_5/providers/NavStateProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AdaptiveScaffold extends StatelessWidget {
   final Widget body;
-  const AdaptiveScaffold({super.key, required this.body});
+  final PageController pageViewController;
+  const AdaptiveScaffold({
+    super.key,
+    required this.body,
+    required this.pageViewController,
+  });
 
   // Use a riverpod provider to handle currently selected state
 
@@ -24,79 +31,91 @@ class AdaptiveScaffold extends StatelessWidget {
           // backgroundColor: Colors.red,
           body: body,
           bottomNavigationBar: isWideScreen == false
-              ? BottomNavigationBar(
-                  onTap: (value) {
-                    print("Navigation tapped: $value");
-                  },
-                  backgroundColor: Colors.transparent,
-                  currentIndex: 3,
-                  showSelectedLabels: false, // do not show label text
-                  showUnselectedLabels: false,
-                  items: const <BottomNavigationBarItem>[
-                    // TODO: update icons, figure out logic for varying colors when selected
+              ? Consumer(builder: (context, ref, child) {
+                  final selectedIndex = ref.watch(navStateProvider);
+                  return BottomNavigationBar(
+                    onTap: (value) {
+                      print("Navigation tapped: $value");
+                      pageViewController.animateToPage(
+                        value,
+                        duration: const Duration(seconds: 1),
+                        curve:
+                            Curves.easeInOutCubicEmphasized, // I like this one
+                      );
+                      // Update the state so the visual representation of the nav
+                      // bar updates too
+                      ref.read(navStateProvider.notifier).setIndex(value);
+                    },
+                    backgroundColor: Colors.transparent,
+                    currentIndex: selectedIndex,
+                    showSelectedLabels: false, // do not show label text
+                    showUnselectedLabels: false,
+                    items: const <BottomNavigationBarItem>[
+                      // TODO: update icons, figure out logic for varying colors when selected
 
-                    // Dashboard
-                    BottomNavigationBarItem(
-                      label: "Dashboard",
-                      activeIcon: Icon(
-                        Icons.dashboard_rounded,
-                        color: Colors.white,
-                        size: 40,
+                      // Dashboard
+                      BottomNavigationBarItem(
+                        label: "Dashboard",
+                        activeIcon: Icon(
+                          Icons.dashboard_rounded,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        icon: Icon(
+                          Icons.dashboard_rounded,
+                          color: Color(0xff5A5969),
+                          size: 40,
+                        ),
                       ),
-                      icon: Icon(
-                        Icons.dashboard_rounded,
-                        color: Color(0xff5A5969),
-                        size: 40,
-                      ),
-                    ),
 
-                    // Attendance
-                    BottomNavigationBarItem(
-                      label: "Attendance",
-                      activeIcon: Icon(
-                        Icons.calendar_month_rounded,
-                        color: Color(0xffE7F98B),
-                        size: 40,
+                      // Attendance
+                      BottomNavigationBarItem(
+                        label: "Attendance",
+                        activeIcon: Icon(
+                          Icons.calendar_month_rounded,
+                          color: Color(0xffE7F98B),
+                          size: 40,
+                        ),
+                        icon: Icon(
+                          Icons.calendar_month_rounded,
+                          color: Color(0xff5A5969),
+                          size: 40,
+                        ),
                       ),
-                      icon: Icon(
-                        Icons.calendar_month_rounded,
-                        color: Color(0xff5A5969),
-                        size: 40,
-                      ),
-                    ),
 
-                    // Vault
-                    BottomNavigationBarItem(
-                      label: "Vault",
-                      activeIcon: Icon(
-                        Icons.add_box_rounded,
-                        color: Color(0xff21D398),
-                        size: 40,
+                      // Vault
+                      BottomNavigationBarItem(
+                        label: "Vault",
+                        activeIcon: Icon(
+                          Icons.add_box_rounded,
+                          color: Color(0xff21D398),
+                          size: 40,
+                        ),
+                        icon: Icon(
+                          Icons.add_box_rounded,
+                          color: Color(0xff5A5969),
+                          size: 40,
+                        ),
                       ),
-                      icon: Icon(
-                        Icons.add_box_rounded,
-                        color: Color(0xff5A5969),
-                        size: 40,
-                      ),
-                    ),
 
-                    // Settings or Config
-                    BottomNavigationBarItem(
-                      label: "Config",
-                      activeIcon: Icon(
-                        Icons.settings,
-                        color: Color(0xff15A3F0),
-                        size: 40,
+                      // Settings or Config
+                      BottomNavigationBarItem(
+                        label: "Config",
+                        activeIcon: Icon(
+                          Icons.settings,
+                          color: Color(0xff15A3F0),
+                          size: 40,
+                        ),
+                        icon: Icon(
+                          Icons.settings,
+                          // color: Colors.red,
+                          color: Color(0xff5A5969),
+                          size: 40,
+                        ),
                       ),
-                      icon: Icon(
-                        Icons.settings,
-                        // color: Colors.red,
-                        color: Color(0xff5A5969),
-                        size: 40,
-                      ),
-                    ),
-                  ],
-                )
+                    ],
+                  );
+                })
               : null,
         );
       },
